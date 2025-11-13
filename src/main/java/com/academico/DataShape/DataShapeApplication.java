bb8a701420805a502984433d1ed3d2d38a59bd06
Lucas Alves <lucas1901alves@gmail.com>
2025-11-13 12:46:32 -0300

    feat(directories): Spring init directories

---
+++ src/main/java/com/academico/DataShape/DataShapeApplication.java
@@ -0,0 +1,13 @@
+package com.academico.DataShape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataShapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataShapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academico/DataShape/DataShapeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academico.DataShape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataShapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
