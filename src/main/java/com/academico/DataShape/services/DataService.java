a18bbcae728d33828368bf7509d76c6881b637d3
Lucas Alves <lucas1901alves@gmail.com>
2025-11-15 00:33:43 -0300

    feat(service): DataService

---
+++ src/main/java/com/academico/DataShape/services/DataService.java
@@ -0,0 +1,10 @@
+package com.academico.DataShape.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataService {
+
+    //Metodos para processar e dividir dados entre Obra e Venda
+    
+}
