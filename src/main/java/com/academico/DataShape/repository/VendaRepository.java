b5937f359134d131e490a9f505c8aaebee166ea9
Lucas Alves <lucas1901alves@gmail.com>
2025-11-17 23:53:24 -0300

    feat(query): query para listar todas as vendas

--- src/main/java/com/academico/DataShape/repository/VendaRepository.java
+++ src/main/java/com/academico/DataShape/repository/VendaRepository.java
@@ -1,10 +1,21 @@
 package com.academico.DataShape.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.academico.DataShape.model.dto.responses.findAllVendasDTO;
 import com.academico.DataShape.model.entity.Venda;
 
 public interface VendaRepository extends JpaRepository<Venda, Long> {
 
+    @Query(""" 
+        SELECT new com.academico.DataShape.model.dto.responses.findAllVendasDTO(
+        o.tituloObra, v.dataVenda, v.valorPago)
+        FROM Venda v
+        JOIN v.obra o
+                """)
+    List<findAllVendasDTO> findAllVendas();
     
 }
