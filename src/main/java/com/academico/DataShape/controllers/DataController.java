04fc0dacccc146fcdfec1a9d17a1231e6a060eca
Lucas Alves <lucas1901alves@gmail.com>
2025-11-20 13:33:57 -0300

    refactor(import): removendo imports desnecessários

--- src/main/java/com/academico/DataShape/controllers/DataController.java
+++ src/main/java/com/academico/DataShape/controllers/DataController.java
@@ -2,7 +2,6 @@ package com.academico.DataShape.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestParam;
