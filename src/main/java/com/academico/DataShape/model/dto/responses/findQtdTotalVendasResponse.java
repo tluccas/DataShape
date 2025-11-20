dfc1cfe6091a550ce4936f8697fffd1238e23bf0
Lucas Alves <lucas1901alves@gmail.com>
2025-11-20 13:38:57 -0300

    feat(response): DTO para response de quantidade total de vendas por obra

---
+++ src/main/java/com/academico/DataShape/model/dto/responses/findQtdTotalVendasResponse.java
@@ -0,0 +1,5 @@
+package com.academico.DataShape.model.dto.responses;
+import java.util.List;
+public record findQtdTotalVendasResponse(Integer total_un_vendidas, List<QtdVendaDTO> qtd_vendas_por_obra) {
+    
+}
